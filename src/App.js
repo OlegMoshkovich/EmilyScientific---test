e95b3cbf2e4d02160e540f377b4cdd6c3783b9c5
Oleg Moshkovich <oleg.mosh@gmail.com>
2019-01-12 02:40:57 -0500

    added sortable table and two ui buttons

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
 import React, { Component } from 'react';
 import CSVReader from "react-csv-reader";
-import Row from './Components/Row'
-import SimpleTable from './Components/MaterialTable'
+// import Row from './Components/Row'
+// import SimpleTable from './Components/MaterialTable'
+import EnhancedTable from './Components/SortableTable'
 import './App.css';
+import Button from '@material-ui/core/Button'
+
+import {Collapse} from 'react-collapse';
+// import {Presets} from 'react-motion';
+// import { Chart } from "react-google-charts";
+
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      csvdata: []
+      isOpened:false,
+      csvData: [],
+      graphData:[]
     }
     this.loadFile = this.loadFile.bind(this);
-    this.resetState = this.resetState.bind(this);
+    this.buttonNameHandler = this.buttonNameHandler.bind(this);
   }
-  resetState(){
-    this.setState({csvdata:[]})
-  }
+
   loadFile(data){
-    this.resetState();
+    const graphData = [];
+    let newArr =[];
+    for (let row in data){
+      if(row === 0){
+        newArr = [data[row][1],data[row][2]]
+      }else{
+        newArr = [parseInt(data[row][1]),parseInt(data[row][2])]
+      }
+
+      graphData.push(newArr)
+      // console.log('printing the row',data[row][1])
+    }
+    console.log('graphingData',graphData)
     this.setState({
-      csvdata: data
+      csvData: data,
+      graphData:graphData
     })
-    document.querySelector('.newTable').style.display = "block"
+    // console.log('loadfile data',data)
+
   };
 
+  buttonNameHandler(){
+     !this.state.isOpened ? this.setState({isOpened:true}):this.setState({isOpened:false})
+  }
+
+
   render() {
+    let buttonTitle;
+    !this.state.isOpened ? buttonTitle = 'Open the table' : buttonTitle = 'Collapse the table'
     return (
       <div className="App">
       <div className="container">
@@ -36,8 +64,33 @@ class App extends Component {
          />
        </div>
        <div className = 'newTable'>
-       <SimpleTable data = {this.state.csvdata}/>
+      {/* <SimpleTable data = {this.state.csvData}/> */}
        </div>
+       <div >
+         <Button variant="contained" color="secondary"onClick={this.buttonNameHandler}>
+            {buttonTitle}
+         </Button>
+       </div>
+       <div style = {{marginTop:10,paddingTop:20}}>
+         <Button variant="contained" color="secondary" onClick={this.populateGraph}>
+            graph
+         </Button>
+       </div>
+       <Collapse isOpened={this.state.isOpened}>
+        <EnhancedTable data = {this.state.csvData}/>
+       </Collapse>
+
+       {/*
+         <Chart
+           chartType="ScatterChart"
+           data={this.state.graphData}
+           width="100%"
+           height="400px"
+           legendToggle
+         />
+         */}
+
+
 
 
       </div>
